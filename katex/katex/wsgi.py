187f66eacbd0f53efcc4b64ce76dc6deb9cc0eab
“businesskaniki” <businesskanikii@gmail.com>
2023-09-12 19:59:01 +0300

    updating requirments

--- katex/katex/wsgi.py
+++ katex/katex/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'katex.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'katex.katex.settings')
 
 application = get_wsgi_application()
