80596ded5a666798cc5d05749f12f51647f4dce7
“businesskaniki” <businesskanikii@gmail.com>
2023-10-04 17:36:57 +0300

    adding admin

--- build.sh
+++ build.sh
@@ -2,35 +2,3 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
-until python -c "import django; django.setup()" 2>/dev/null; do
-  >&2 echo "Waiting for Django to initialize..."
-  sleep 1
-done
-
-export DJANGO_SETTINGS_MODULE=katex.settings
-
-
-python <<EOF
-import os
-import django
-from django.contrib.auth.models import User
-
-# Initialize Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'katex.settings')
-django.setup()
-
-# Create superuser
-def create_superadmin():
-    username = 'nicholas'
-    email = 'businesskaniki@gmail.com'
-    password = '!Kaniki1234'
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
-        print(f'Superuser {username} created successfully!')
-    else:
-        print(f'Superuser {username} already exists.')
-
-if __name__ == '__main__':
-    create_superadmin()
-EOF
