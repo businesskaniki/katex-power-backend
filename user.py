fef08e3c7f59df8c19926d898d3fe847a56935ce
“businesskaniki” <businesskanikii@gmail.com>
2023-10-04 18:21:08 +0300

    modifying setupsh

--- user.py
+++ user.py
@@ -2,18 +2,18 @@ import os
 import django
 
 # Set the DJANGO_SETTINGS_MODULE environment variable
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'katex.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "katex.settings")
 
 # Initialize Django
 django.setup()
 
 # Now you can import and work with Django models
-from django.contrib.auth.models import User
-
+from katexpower.models import UserProfile
 
 
 # Replace 'nicholasmaina' with the actual username of your superuser
-user = User.objects.get(username='nicholasmaina')
+user = UserProfile.objects.get(username="nicholasmaina")
+
 
 # Print the password
 print(user.password)
