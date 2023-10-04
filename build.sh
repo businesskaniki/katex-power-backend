5241b0047df814af136a44c37749e92e6315bbf3
“businesskaniki” <businesskanikii@gmail.com>
2023-10-04 17:44:24 +0300

    adding admin

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
-python manage.py createsuperuser --username=nicholas --email=businesskaniki@gmail.com --noinput --password=!Kaniki1234
-
+python manage.py createsuperuser  --noinput --username=nicholas  --noinput --email=businesskaniki@gmail.com
