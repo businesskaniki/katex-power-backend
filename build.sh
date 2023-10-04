cbd237b316ce8695f7f55618ec1c00f60cd34d7d
“businesskaniki” <businesskanikii@gmail.com>
2023-10-04 18:37:50 +0300

    modifying setupsh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
-# python manage.py createsuperuser  --noinput --username=nicholasmaina  --noinput --email=businesskanikii@gmail.com
+python manage.py createsuperuser  --noinput --username=nicholasmaina  --noinput --email=businesskanikii@gmail.com
 python user.py
