0d0892b4286367a7fdb01b0650b0527bfda7162f
“businesskaniki” <businesskanikii@gmail.com>
2023-10-04 20:33:53 +0300

    add admin in shell

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ python manage.py createsuperuser --noinput --username=kachi2032  --email=kachi32
 
 # Start Django shell and execute Python code to create a superuser with a password
 python manage.py shell <<EOF
-    from katexpower.models import UserProfile
-    user = UserProfile.objects.get('kachi2032')
-    user.set_password('!Kaniki1234')
-    user.save()
+from katexpower.models import UserProfile
+user = UserProfile.objects.get('kachi2032')
+user.set_password('!Kaniki1234')
+user.save()
 EOF
 
