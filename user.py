64e7d85fd1240e0548f6008de33e69450c368433
“businesskaniki” <businesskanikii@gmail.com>
2023-10-04 18:12:24 +0300

    modifying setupsh

--- user.py
+++ user.py
@@ -1,4 +1,16 @@
+import os
+import django
+
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'katex.settings')
+
+# Initialize Django
+django.setup()
+
+# Now you can import and work with Django models
 from django.contrib.auth.models import User
+
+
 
 # Replace 'nicholasmaina' with the actual username of your superuser
 user = User.objects.get(username='nicholasmaina')
